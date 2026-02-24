0377422a12aef27efa026005962cf2457821f55d
Ryan Lim <1477135+ryanlim@users.noreply.github.com>
2026-02-23 19:47:56 -0800

    Make tailscale_up more resilient.

--- tailscale/scripts/tailscale_up.sh
+++ tailscale/scripts/tailscale_up.sh
@@ -17,12 +17,12 @@ if [ -n "$TAILSCALE_UP_LOGIN_SERVER" ]; then
   LOGIN_SERVER="--login-server $TAILSCALE_UP_LOGIN_SERVER"
   tailscale up --advertise-exit-node --hostname $INSTANCE_NAME_ --login-server $TAILSCALE_UP_LOGIN_SERVER
 else
-  tailscale up --advertise-exit-node --hostname $INSTANCE_NAME_ $LOGIN_SERVER 
+  tailscale up --advertise-exit-node --hostname $INSTANCE_NAME_ $LOGIN_SERVER
 fi
 
 apk add mtr curl prometheus-node-exporter tinyproxy
 
-cat <<EOF > /etc/tinyproxy.conf
+cat <<EOF >/etc/tinyproxy.conf
 Port 80
 Listen 0.0.0.0
 Timeout 600
@@ -31,9 +31,13 @@ EOF
 
 tinyproxy -c /etc/tinyproxy.conf
 
-nohup /usr/bin/node_exporter > /tmp/node_exporter.log 2>&1 &
+nohup /usr/bin/node_exporter >/tmp/node_exporter.log 2>&1 &
 
 while [ 1 ]; do
   sleep 60
+  pidof tailscaled
+  if [ $? -ne 0 ]; then
+    tailscaled &
+  fi
   date
 done
